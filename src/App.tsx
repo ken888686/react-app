2ff7055d8bdc25092ede78ed7b79b27ad666246c
Yungchun Tu <ken888686@gmail.com>
2023-06-19 15:22:12 +0900

    update folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,25 @@
 import './App.scss'
-import Card from './components/Card'
-import ExpenseItem from './components/ExpenseItem'
+import Card from './components/UI/Card'
+import ExpenseItem from './components/Expenses/ExpenseItem'
 
 const data = [
-  {
-    date: new Date(1993, 9, 3),
-    title: 'Aaron',
-    amount: 100,
-  },
-  {
-    date: new Date(1994, 10, 4),
-    title: 'Belle',
-    amount: 100,
-  },
-  {
-    date: new Date(1995, 11, 5),
-    title: 'Carlin',
-    amount: 100,
-  },
-  {
-    date: new Date(1996, 12, 6),
-    title: 'David',
-    amount: 100,
-  },
-  {
-    date: new Date(1997, 1, 7),
-    title: 'Elise',
-    amount: 100,
-  },
+  { id: 0, date: new Date(1993, 9, 3), title: 'Aaron', amount: 100 },
+  { id: 1, date: new Date(1994, 10, 4), title: 'Belle', amount: 100 },
+  { id: 2, date: new Date(1995, 11, 5), title: 'Carlin', amount: 100 },
+  { id: 3, date: new Date(1996, 12, 6), title: 'David', amount: 100 },
+  { id: 4, date: new Date(1997, 1, 7), title: 'Elise', amount: 100 },
 ]
 
 function App() {
   return (
     <Card className="expenses">
       {data.map((x) => (
-        <ExpenseItem date={x.date} title={x.title} amount={x.amount} />
+        <ExpenseItem
+          key={x.id}
+          date={x.date}
+          title={x.title}
+          amount={x.amount}
+        />
       ))}
     </Card>
   )
